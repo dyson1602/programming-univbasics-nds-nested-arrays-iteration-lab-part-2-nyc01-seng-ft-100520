04d6469d0306077d86fbf8de35789cd2bb54cccb
Christopher Clark <cclark000@citymail.cuny.edu>
2020-09-14 19:06:26 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,21 +1,18 @@
 def find_min_in_nested_arrays(src)
   
   row_index = 0
+  weekly_low_temps = []
   
   while row_index < src.count do
-    
     element_index = 0
-    
+    low_temp = 1000
     while element_index < src[row_index].count do
-      
-      
-      if src[row_index][element_index] do
-        
+      if src[row_index][element_index] < low_temp
+        low_temp = src[row_index][element_index]
       end
-      
     element_index += 1
     end
-    
+  weekly_low_temps << low_temp  
   row_index += 1
   end
 
